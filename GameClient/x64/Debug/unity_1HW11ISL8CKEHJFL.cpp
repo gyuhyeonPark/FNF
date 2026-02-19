e87e016b81b3d75f4dc36579b4412faed56eb56f
gyuhyeonPark <gmfrwh12@gmail.com>
2026-02-19 15:09:27 +0900

    연동 완룡

---
+++ GameClient/x64/Debug/unity_1HW11ISL8CKEHJFL.cpp
@@ -0,0 +1,265 @@
+#include "pch.h"
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AFlipbook.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AGrapicShader.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AMaterial.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AMesh.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AScene.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ASprite.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Asset.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AssetManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\AssetManager_Init.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ATexture.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ATileMap.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CameraUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CBillboardRenderer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CCamera.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CCameraMove.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CCollider2D.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CFlipbookRenderer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CharacterController.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CLight2D.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CMissileScript.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Collider2DUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CollisionManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ComponentUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\COpponentController.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CPlayerScript.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CRenderComponent.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CSpriteRenderer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CTileRenderer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\EditorManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\EditorUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\EventManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\extern.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\FlipbookRendererUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\func.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_demo.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_draw.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_impl_dx11.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_impl_win32.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_tables.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\imgui_widgets.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Inspector.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Inspector_Init.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Light2DUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ListUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Menu.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\MeshRendererUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Note.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\OpponentNoteReceptor.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Outliner.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Parser.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CAtlasAnimator.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CImage.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CMeshRenderer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Component.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\ConstantBuffer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CPlayerController.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CScript.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\CTransform.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Dotween.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\DotweenTimer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Easing.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Engine.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Engine_Init.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Entity.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\GameClient.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\GameManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\GameObject.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Graphics.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\KeyManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\Layer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\NoteReceptor.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\PathManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\IResourceManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\PlayerNoteReceptor.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\RenderManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\SceneManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\SimpleMath.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\SingletonClass.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\SoundManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\SpriteRendererUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\StartingPistol.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\StructuredBuffer.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\TaskManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\TileRendererUI.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\TimeManager.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\tinyxml2.cpp"
+
+
+#include "C:\Users\Parkgyuhyeon\FNF\GameClient\TransformUI.cpp"
+
